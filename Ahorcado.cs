e835002d10b2d7da53fd691a6d4a980943f1081a
NikolaiBelic <57221840+NikolaiBelic@users.noreply.github.com>
2021-05-06 10:35:29 +0200

    Add files via upload

---
+++ Ahorcado.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ProjectX
+{
+    class Ahorcado
+    {
+        List<string> palabras;
+        int indice = 0;
+        string estado;
+        int estadoAhorcado;
+        public Ahorcado()
+        {
+            palabras = new List<string>();
+
+            palabras.Add("intermitente");
+            palabras.Add("farola");
+            palabras.Add("olivo");
+            palabras.Add("rupestre");
+            palabras.Add("policia");
+            palabras.Add("tenista");
+            palabras.Add("exterminador");
+            palabras.Add("sillon");
+            palabras.Add("garaje");
+            palabras.Add("boquete");
+            palabras.Add("geranio");
+            palabras.Add("bombilla");
+            palabras.Add("electricidad");
+            palabras.Add("jabali");
+            palabras.Add("persiana");
+            palabras.Add("sulfato");
+            palabras.Add("mentecato");
+
+            estadoAhorcado = 6;
+        }
+
+        public string GetEstado()
+        {
+            return estado;
+        }
+
+        public void SetEstado(string nuevoEstado)
+        {
+            estado = nuevoEstado;
+        }
+
+        public int GetIndice()
+        {
+            return indice;
+        }
+
+        public void SetIndice(int nuevoIndice)
+        {
+            indice = nuevoIndice;
+        }
+
+        public void Jugar()
+        {
+            Random numeroAleatorio = new Random();
+            string letra;
+
+            indice = numeroAleatorio.Next(0, palabras.Count);
+
+            DibujaPalabra();
+
+            Console.WriteLine("");
+
+            while (estadoAhorcado > 0 && GetEstado() != palabras[indice])
+            {
+                Console.Write("Introduzca letra: ");
+                letra = Console.ReadLine();
+
+                ActualizarPalabra(letra);
+
+                Console.WriteLine("");
+
+                if (estadoAhorcado == 0)
+                {
+                    Console.WriteLine("¡PERDISTE!");
+                    Thread.Sleep(3000);
+                }
+
+                if (GetEstado() == palabras[indice])
+                {
+                    Console.WriteLine("¡HAS GANADO!");
+                    Thread.Sleep(3000);
+                }
+            }
+
+            SetIndice(6);
+            Console.Clear();
+        }
+
+        public void AddPalabra(string nuevaPalabra)
+        {
+            palabras.Add(nuevaPalabra);
+        }
+
+        public void DibujaPalabra()
+        {
+            estado = new string('_', palabras[indice].Length);
+            Console.Write(estado);
+        }
+
+        public void DibujaAhorcado()
+        {
+            estadoAhorcado -= 1;
+        }
+
+        public void ActualizarPalabra(string letra)
+        {
+            int letraBuscar, letraBuscar1 = 0;
+            string subcadenaEstado;
+
+            letraBuscar = palabras[indice].IndexOf(letra);
+
+            if (letraBuscar >= 0)
+            {
+                estado = estado.Remove(letraBuscar, 1);
+                estado = estado.Insert(letraBuscar, letra);
+
+                SetEstado(estado);
+
+                while (letraBuscar1 >= 0 && letraBuscar <= estado.Length - 1)
+                {
+                    subcadenaEstado = palabras[indice].Substring(letraBuscar + 1, (estado.Length - 1) - (letraBuscar));
+                    letraBuscar1 = subcadenaEstado.IndexOf(letra);
+
+                    if (letraBuscar1 >= 0)
+                    {
+                        estado = estado.Remove(letraBuscar + letraBuscar1 + 1, 1);
+                        estado = estado.Insert(letraBuscar + letraBuscar1 + 1, letra);
+
+                        SetEstado(estado);
+
+                        letraBuscar += letraBuscar1 + 2;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("LA PALABRA NO CONTIENE LA LETRA \"{0}\"", letra);
+                estadoAhorcado -= 1;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Te quedan {0} vidas", estadoAhorcado);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.Write(GetEstado());
+
+            // + " | ");
+            // Console.Write(palabras[indice]);
+
+            Console.WriteLine("");
+        }
+
+        public void MostrarPalabras()
+        {
+            foreach (string s in palabras)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ EsquivaFlechas.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ProjectX
+{
+    class EsquivaFlechas
+    {
+        public string flechaIzq = "<--";
+        public string flechaDcha = "-->";
+        List<string> flechasDcha;
+
+        public EsquivaFlechas()
+        {
+            this.flechaDcha = "-->";
+            this.flechaIzq = "<--";
+            flechasDcha = new List<string>();
+        }
+
+        public string GetFlechaDcha()
+        {
+            return flechaDcha;
+        }
+
+        public void SetFlechaDcha(string nuevaFlechaDcha)
+        {
+            flechaDcha = nuevaFlechaDcha;
+        }
+
+        /*   public void MoverFlechas()
+           {
+               int ancho = Console.WindowWidth;
+               int alto = Console.WindowHeight;
+
+               for (int i = 0; i <= ancho; i++)
+               {
+                   Console.Clear();
+                   Console.SetCursorPosition(i, 10);
+                   Console.WriteLine(flechaDcha);
+                   Thread.Sleep(500);
+               }
+           }*/
+
+        public void Mapa()
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j < 101; j++)
+                {
+                    if ((i > 1 && i < 24) && (j > 2 && j < 98))
+                    {
+                        Console.SetCursorPosition(j, i);
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(j, i);
+                        Console.Write("#");
+                    }
+                }
+            }
+        }
+
+        public void MovimientoFlechaDcha()
+        {
+            Random y = new Random();
+
+            int altura = y.Next(3, 24);
+
+            for(int i = 1; i < 100; i++)
+            {
+                if (i == 99)
+                {
+                    i = 1;
+                    SetFlechaDcha("-->");
+                }
+                if (i <= 3)
+                {
+                    Console.SetCursorPosition(i - 1, altura);
+                    Console.Write("#");
+                }
+                if (i > 3)
+                {
+                    Console.SetCursorPosition(i - 1, altura);
+                    Console.Write(" ");
+                }
+
+                if (i > 95)
+                {
+                    Console.SetCursorPosition(i, altura);
+                    Console.Write(CortarFlechaDcha(GetFlechaDcha()));
+                    SetFlechaDcha(CortarFlechaDcha(GetFlechaDcha()));
+                }
+                
+                Console.SetCursorPosition(i, altura);
+                Console.Write(GetFlechaDcha());
+                Thread.Sleep(10);
+            }
+            
+
+        }
+
+        public string CortarFlechaDcha(string felchaDcha)
+        {
+            string nuevaFlecha = "";
+
+            for (int i = 0; i < flechaDcha.Length - 1; i++)
+            {
+                nuevaFlecha += Convert.ToString(flechaDcha[i]);
+            }
+
+            return nuevaFlecha;
+        }
+    }
+}
